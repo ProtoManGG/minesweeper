6f1abee2a67c08934b344aa9652b5bb6db319e05
ProtoManGG <pranav.taneja36@gmail.com>
2024-08-16 19:06:56 +0530

    refactor

--- lib/game_logic.dart
+++ lib/game_logic.dart
@@ -1,3 +1,5 @@
+import 'package:flutter/material.dart';
+
 class GameLogic {
   static const int numberOfSquares = 9 * 9;
   static const int numberInEachRow = 9;
@@ -18,20 +20,175 @@ class GameLogic {
   }
 
   void scanBombs() {
-    // Logic to scan bombs and update squareStatus
+    for (int i = 0; i < numberOfSquares; i++) {
+      int numberOfBombsAround = 0;
+
+      // Check surrounding squares for bombs
+      final bool isNotFirstColumn = i % numberInEachRow != 0;
+      final bool isNotLastColumn = (i + 1) % numberInEachRow != 0;
+      final bool isNotFirstRow = i >= numberInEachRow;
+      final bool isNotLastRow = !(i >= (numberOfSquares - numberInEachRow) &&
+          i <= (numberOfSquares - 1));
+
+      // Check each surrounding square
+      if (isNotFirstColumn && bombLocation.contains(i - 1)) {
+        numberOfBombsAround++;
+      }
+      if (isNotFirstColumn &&
+          isNotFirstRow &&
+          bombLocation.contains(i - 1 - numberInEachRow)) numberOfBombsAround++;
+      if (isNotFirstRow && bombLocation.contains(i - numberInEachRow)) {
+        numberOfBombsAround++;
+      }
+      if (isNotLastColumn &&
+          isNotFirstRow &&
+          bombLocation.contains(i + 1 - numberInEachRow)) numberOfBombsAround++;
+      if (isNotLastColumn && bombLocation.contains(i + 1)) {
+        numberOfBombsAround++;
+      }
+      if (isNotLastColumn &&
+          isNotLastRow &&
+          bombLocation.contains(i + 1 + numberInEachRow)) numberOfBombsAround++;
+      if (isNotLastRow && bombLocation.contains(i + numberInEachRow)) {
+        numberOfBombsAround++;
+      }
+      if (isNotFirstColumn &&
+          isNotLastRow &&
+          bombLocation.contains(i - 1 + numberInEachRow)) numberOfBombsAround++;
+
+      squareStatus[i] = {
+        'numOfBombsAround': numberOfBombsAround,
+        'isRevealed': squareStatus[i]['isRevealed']
+      };
+    }
   }
 
   void revealBox(int index) {
-    // Logic to reveal a box
+    if (squareStatus[index]['numOfBombsAround'] > 0) {
+      squareStatus[index] = {
+        'numOfBombsAround': squareStatus[index]['numOfBombsAround'],
+        'isRevealed': true
+      };
+    } else {
+      // Logic to reveal surrounding boxes
+      revealSurroundingBoxes(index);
+    }
+  }
+
+  void revealSurroundingBoxes(int index) {
+    // Check if the box is already revealed
+    if (squareStatus[index]['isRevealed']) return;
+
+    // Reveal the current box
+    squareStatus[index] = {
+      'numOfBombsAround': squareStatus[index]['numOfBombsAround'],
+      'isRevealed': true
+    };
+
+    // If there are bombs around, do not reveal surrounding boxes
+    if (squareStatus[index]['numOfBombsAround'] > 0) return;
+
+    // Determine the surrounding indices
+    final bool isNotFirstColumn = index % numberInEachRow != 0;
+    final bool isNotLastColumn = (index + 1) % numberInEachRow != 0;
+    final bool isNotFirstRow = index >= numberInEachRow;
+    final bool isNotLastRow = !(index >= (numberOfSquares - numberInEachRow) &&
+        index <= (numberOfSquares - 1));
+
+    // Reveal surrounding boxes recursively
+    if (isNotFirstColumn) revealSurroundingBoxes(index - 1); // Left
+    if (isNotLastColumn) revealSurroundingBoxes(index + 1); // Right
+    if (isNotFirstRow) revealSurroundingBoxes(index - numberInEachRow); // Top
+    if (isNotLastRow) revealSurroundingBoxes(index + numberInEachRow); // Bottom
+    if (isNotFirstColumn && isNotFirstRow) {
+      revealSurroundingBoxes(index - 1 - numberInEachRow); // Top Left
+    }
+    if (isNotLastColumn && isNotFirstRow) {
+      revealSurroundingBoxes(index + 1 - numberInEachRow); // Top Right
+    }
+    if (isNotFirstColumn && isNotLastRow) {
+      revealSurroundingBoxes(index - 1 + numberInEachRow); // Bottom Left
+    }
+    if (isNotLastColumn && isNotLastRow) {
+      revealSurroundingBoxes(index + 1 + numberInEachRow); // Bottom Right
+    }
   }
 
   void restartGame() {
-    // Logic to restart the game
+    bombsRevealed = false;
+    for (var i = 0; i < numberOfSquares; i++) {
+      squareStatus[i] = {
+        'numOfBombsAround': squareStatus[i]['numOfBombsAround'],
+        'isRevealed': false
+      };
+    }
+    initializeGame();
   }
 
   bool isBomb(int index) {
     return bombLocation.contains(index);
   }
 
-  // Other game-related methods...
+  void playerLost(BuildContext context, VoidCallback restartGame) {
+    showDialog(
+      context: context,
+      builder: (context) {
+        return AlertDialog(
+          backgroundColor: Colors.grey[700],
+          title: const Text(
+            'YOU LOST!',
+            style: TextStyle(color: Colors.white),
+          ),
+          actions: [
+            ElevatedButton(
+              onPressed: () {
+                restartGame();
+                Navigator.pop(context);
+              },
+              child: const Icon(Icons.restart_alt),
+            ),
+          ],
+        );
+      },
+    );
+  }
+
+  void playerWon(BuildContext context, VoidCallback restartGame) {
+    showDialog(
+      context: context,
+      builder: (context) {
+        return AlertDialog(
+          backgroundColor: Colors.grey[700],
+          title: const Text(
+            'YOU WON!',
+            style: TextStyle(color: Colors.white),
+          ),
+          actions: [
+            ElevatedButton(
+              onPressed: () {
+                restartGame();
+                Navigator.pop(context);
+              },
+              child: const Icon(Icons.restart_alt),
+            ),
+          ],
+        );
+      },
+    );
+  }
+
+  void checkWinner(BuildContext context, VoidCallback restartGame) {
+    //check how many boxes yet to be revealed
+    int boxesYetToBeRevealed = 0;
+
+    for (var i = 0; i < numberOfSquares; i++) {
+      if (squareStatus[i]['isRevealed'] == false) {
+        boxesYetToBeRevealed++;
+      }
+    }
+
+    if (boxesYetToBeRevealed == bombLocation.length) {
+      playerWon(context, restartGame);
+    }
+  }
 }
--- lib/homepage.dart
+++ lib/homepage.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:minesweeper/bomb.dart';
 import 'package:minesweeper/number_box.dart';
+import 'game_logic.dart'; // Import the new GameLogic class
 
 class Homepage extends StatefulWidget {
   const Homepage({super.key});
@@ -10,28 +11,7 @@ class Homepage extends StatefulWidget {
 }
 
 class _HomepageState extends State<Homepage> {
-  //variables
-  static const int numberOfSquares = 9 * 9;
-  static const int numberInEachRow = 9;
-
-  //[(num of bombs around, are they revealed)]
-  final List<({int numOfBombsAround, bool isRevealed})> squareStatus = [];
-
-  // bomb locations
-  final List<int> bombLocation = [4, 36, 37, 38, 39, 40, 41, 61];
-
-  bool bombsRevealed = false;
-
-  @override
-  void initState() {
-    //initially each square has 0 bombs and is unrevealed
-    for (int i = 0; i < numberOfSquares; i++) {
-      squareStatus.add((numOfBombsAround: 0, isRevealed: false));
-    }
-
-    scanBombs();
-    super.initState();
-  }
+  final GameLogic gameLogic = GameLogic(); // Create an instance of GameLogic
 
   @override
   Widget build(BuildContext context) {
@@ -39,10 +19,9 @@ class _HomepageState extends State<Homepage> {
       backgroundColor: Colors.grey[200],
       body: Column(
         children: [
-          //game stats and menu
+          // UI code for displaying game stats and menu
           SizedBox(
             height: 150 + MediaQuery.of(context).padding.top,
-            // color: Colors.grey,
             child: Row(
               mainAxisAlignment: MainAxisAlignment.spaceEvenly,
               children: [
@@ -51,7 +30,7 @@ class _HomepageState extends State<Homepage> {
                   mainAxisAlignment: MainAxisAlignment.center,
                   children: [
                     Text(
-                      bombLocation.length.toString(),
+                      gameLogic.bombLocation.length.toString(),
                       style: const TextStyle(fontSize: 40),
                     ),
                     const Text('B O M B'),
@@ -93,29 +72,32 @@ class _HomepageState extends State<Homepage> {
             child: GridView.builder(
               physics: const NeverScrollableScrollPhysics(),
               gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
-                crossAxisCount: numberInEachRow,
+                crossAxisCount: GameLogic.numberInEachRow,
               ),
-              itemCount: numberOfSquares,
+              itemCount: GameLogic.numberOfSquares,
               itemBuilder: (BuildContext context, int index) {
-                if (bombLocation.contains(index)) {
+                if (gameLogic.isBomb(index)) {
                   return Bomb(
-                    revealed: bombsRevealed,
+                    revealed: gameLogic.bombsRevealed,
                     onTap: () {
-                      //if user tapped bomb he loses
                       setState(() {
-                        bombsRevealed = true;
+                        gameLogic.bombsRevealed = true;
+                        gameLogic.playerLost(context, restartGame);
                       });
-                      playerLost();
+                      // Handle bomb tap
                     },
                   );
                 } else {
                   return NumberBox(
-                    child: (squareStatus[index].numOfBombsAround).toString(),
-                    revealed: squareStatus[index].isRevealed,
+                    child: (gameLogic.squareStatus[index]['numOfBombsAround'])
+                        .toString(),
+                    revealed: gameLogic.squareStatus[index]['isRevealed'],
                     onTap: () {
-                      //reveal current box
-                      revealBox(index);
-                      checkWinner();
+                      setState(() {
+                        gameLogic.revealBox(index);
+                        gameLogic.checkWinner(context, restartGame);
+                      });
+                      // Check winner logic
                     },
                   );
                 }
@@ -135,306 +117,9 @@ class _HomepageState extends State<Homepage> {
     );
   }
 
-  void revealBox(int index) {
-    //reveal current box if it is 1,2,3
-    if (squareStatus[index].numOfBombsAround > 0) {
-      setState(() {
-        // Create a new record with the updated value
-        squareStatus[index] = (
-          numOfBombsAround: squareStatus[index].numOfBombsAround,
-          isRevealed: true
-        );
-      });
-    } else {
-      //reveal current box, and the 8 surrounding boxes unless you are on a wall
-
-      final isNotFirstColumn = index % numberInEachRow != 0;
-      final isNotLastColumn = index % numberInEachRow != numberInEachRow - 1;
-      final isNotFirstRow = index >= numberInEachRow;
-      final isNotLastRow = !((index >= numberOfSquares - numberInEachRow) &&
-          (index <= numberOfSquares - 1));
-
-      setState(() {
-        //reveal current box
-        squareStatus[index] = (
-          numOfBombsAround: squareStatus[index].numOfBombsAround,
-          isRevealed: true
-        );
-
-        //revealing left side boxes
-        if (isNotFirstColumn) {
-          // if this next (left) box isn't revealed yet and it is a 0, recurse
-          final indexToCheck = index - 1;
-          if (squareStatus[indexToCheck].numOfBombsAround == 0 &&
-              squareStatus[indexToCheck].isRevealed == false) {
-            revealBox(indexToCheck);
-          }
-
-          // reveal left box
-          squareStatus[indexToCheck] = (
-            numOfBombsAround: squareStatus[indexToCheck].numOfBombsAround,
-            isRevealed: true
-          );
-        }
-
-        // revelaing top left boxes, unless we are on top or leftmost wall
-        if (isNotFirstColumn && isNotFirstRow) {
-          //if top left box isn't revealed and is a 0
-          final indexToCheck = index - 1 - numberInEachRow;
-          if (squareStatus[indexToCheck].numOfBombsAround == 0 &&
-              squareStatus[indexToCheck].isRevealed == false) {
-            //recurse
-            revealBox(indexToCheck);
-          }
-
-          squareStatus[indexToCheck] = (
-            numOfBombsAround: squareStatus[indexToCheck].numOfBombsAround,
-            isRevealed: true
-          );
-        }
-
-        // revealing top boxes, unless we are on top row
-        if (isNotFirstRow) {
-          final indexToCheck = index - numberInEachRow;
-          if (squareStatus[indexToCheck].numOfBombsAround == 0 &&
-              squareStatus[indexToCheck].isRevealed == false) {
-            //recurse
-            revealBox(indexToCheck);
-          }
-
-          squareStatus[indexToCheck] = (
-            numOfBombsAround: squareStatus[indexToCheck].numOfBombsAround,
-            isRevealed: true
-          );
-        }
-
-        // revealing top right boxes, unless we are on top row or last column
-        if (isNotFirstRow && isNotLastColumn) {
-          final indexToCheck = index - numberInEachRow + 1;
-
-          if (squareStatus[indexToCheck].numOfBombsAround == 0 &&
-              squareStatus[indexToCheck].isRevealed == false) {
-            //recurse
-            revealBox(indexToCheck);
-          }
-
-          squareStatus[indexToCheck] = (
-            numOfBombsAround: squareStatus[indexToCheck].numOfBombsAround,
-            isRevealed: true
-          );
-        }
-
-        // revealing right boxes, unless we are on last column
-        if (isNotLastColumn) {
-          final indexToCheck = index + 1;
-          if (squareStatus[indexToCheck].numOfBombsAround == 0 &&
-              squareStatus[indexToCheck].isRevealed == false) {
-            //recurse
-            revealBox(indexToCheck);
-          }
-
-          squareStatus[indexToCheck] = (
-            numOfBombsAround: squareStatus[indexToCheck].numOfBombsAround,
-            isRevealed: true
-          );
-        }
-
-        //! revealing bottom right boxes, unless we are on last column or last row
-        if (isNotLastColumn && isNotLastRow) {
-          final indexToCheck = index + 1 + numberInEachRow;
-          if (squareStatus[indexToCheck].numOfBombsAround == 0 &&
-              squareStatus[indexToCheck].isRevealed == false) {
-            //recurse
-            revealBox(indexToCheck);
-          }
-
-          squareStatus[indexToCheck] = (
-            numOfBombsAround: squareStatus[indexToCheck].numOfBombsAround,
-            isRevealed: true
-          );
-        }
-
-        // revealing bottom boxes, unless we are on last row
-        if (isNotLastRow) {
-          final indexToCheck = index + numberInEachRow;
-
-          if (squareStatus[indexToCheck].numOfBombsAround == 0 &&
-              squareStatus[indexToCheck].isRevealed == false) {
-            //recurse
-            revealBox(indexToCheck);
-          }
-
-          squareStatus[indexToCheck] = (
-            numOfBombsAround: squareStatus[indexToCheck].numOfBombsAround,
-            isRevealed: true
-          );
-        }
-
-        // revealing bottom left boxes, unless we are on last row or first column
-        if (isNotFirstColumn && isNotLastRow) {
-          final indexToCheck = index - 1 + numberInEachRow;
-
-          if (squareStatus[indexToCheck].numOfBombsAround == 0 &&
-              squareStatus[indexToCheck].isRevealed == false) {
-            //recurse
-            revealBox(indexToCheck);
-          }
-
-          squareStatus[indexToCheck] = (
-            numOfBombsAround: squareStatus[indexToCheck].numOfBombsAround,
-            isRevealed: true
-          );
-        }
-      });
-    }
-  }
-
-  void scanBombs() {
-    for (int i = 0; i < numberOfSquares; i++) {
-      //there are no bombs around initially = 0
-      int numberOfBombsAround = 0;
-
-      /*
-      check each square if there is a bomb surrounding it
-      there are 8 surrounding boxes to check
-      */
-      final numberNotInFirstColumn = i % numberInEachRow != 0;
-      final numberNotInLastColum = (i + 1) % numberInEachRow != 0;
-      final numberNotInFirstRow = i >= numberInEachRow;
-      final numberNotInLastRow = !(i >= (numberOfSquares - numberInEachRow) &&
-          i <= (numberOfSquares - 1));
-
-      // check square to left, unless it is the first column
-      if (numberNotInFirstColumn && bombLocation.contains(i - 1)) {
-        numberOfBombsAround++;
-      }
-
-      // check square top-left, unless it is the first column or in first row
-      // from current index -1 (to move a column back) -num in each row to move a ro up
-      if (numberNotInFirstColumn &&
-          numberNotInFirstRow &&
-          bombLocation.contains(i - 1 - numberInEachRow)) {
-        numberOfBombsAround++;
-      }
-
-      // check top square, unless in first row
-      if (i >= numberInEachRow && bombLocation.contains(i - numberInEachRow)) {
-        numberOfBombsAround++;
-      }
-
-      // check top right square, unless in first row and last column
-      // from current index +1 (to move a column forward) -num in each row to move a ro up
-      if (numberNotInLastColum &&
-          numberNotInFirstRow &&
-          bombLocation.contains(i + 1 - numberInEachRow)) {
-        numberOfBombsAround++;
-      }
-
-      // check right square, unless we are on last column
-      if (numberNotInLastColum && bombLocation.contains(i + 1)) {
-        numberOfBombsAround++;
-      }
-
-      //check bottom right square unless, we are in last colum and last row
-      if (numberNotInLastColum &&
-          numberNotInLastRow &&
-          bombLocation.contains(i + 1 + numberInEachRow)) {
-        numberOfBombsAround++;
-      }
-
-      // check bottom square unless we are in last row
-      if (numberNotInLastRow && bombLocation.contains(i + numberInEachRow)) {
-        numberOfBombsAround++;
-      }
-
-      // check bottom left , unless we are in last row and first column
-      if (numberNotInFirstColumn &&
-          numberNotInLastRow &&
-          bombLocation.contains(i - 1 + numberInEachRow)) {
-        numberOfBombsAround++;
-      }
-
-      setState(() {
-        squareStatus[i] = (
-          numOfBombsAround: numberOfBombsAround,
-          isRevealed: squareStatus[i].isRevealed,
-        );
-      });
-    }
-  }
-
   void restartGame() {
     setState(() {
-      bombsRevealed = false;
-      for (var i = 0; i < numberOfSquares; i++) {
-        squareStatus[i] = (
-          numOfBombsAround: squareStatus[i].numOfBombsAround,
-          isRevealed: false
-        );
-      }
+      gameLogic.restartGame();
     });
-  }
-
-  void playerLost() {
-    showDialog(
-      context: context,
-      builder: (context) {
-        return AlertDialog(
-          backgroundColor: Colors.grey[700],
-          title: const Text(
-            'YOU LOST!',
-            style: TextStyle(color: Colors.white),
-          ),
-          actions: [
-            ElevatedButton(
-              onPressed: () {
-                restartGame();
-                Navigator.pop(context);
-              },
-              child: const Icon(Icons.restart_alt),
-            ),
-          ],
-        );
-      },
-    );
-  }
-
-  void playerWon(){
-    showDialog(
-      context: context,
-      builder: (context) {
-        return AlertDialog(
-          backgroundColor: Colors.grey[700],
-          title: const Text(
-            'YOU WON!',
-            style: TextStyle(color: Colors.white),
-          ),
-          actions: [
-            ElevatedButton( 
-              onPressed: () {
-                restartGame();
-                Navigator.pop(context);
-              },
-              child: const Icon(Icons.restart_alt),
-            ),
-          ],
-        );
-      },
-    );
-  }
-
-  void checkWinner(){
-    //check how many boxes yet to be revealed
-    int boxesYetToBeRevealed = 0;
-
-    for (var i = 0; i < numberOfSquares; i++) {
-      if(squareStatus[i].isRevealed == false){
-        boxesYetToBeRevealed++;
-      }
-    }
-
-    if(boxesYetToBeRevealed == bombLocation.length){
-      playerWon();
-    }
   }
 }
